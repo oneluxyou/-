318eba9bb2572426c1fed7d4132b3fa353753700
oneluxyou <77523920+oneluxyou@users.noreply.github.com>
2021-11-18 12:04:07 +0800

    Update index.tsx

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,69 +1,55 @@
-import React from 'react';
-import { PageContainer } from "@ant-design/pro-layout";
-import ProTable from "@ant-design/pro-table";
-import {Button,Alert} from "antd";
-import {PlusOutlined} from "@ant-design/icons";
-import {useEffect, useState} from "react";
-import {getTodoLists} from "@/services/todo";
-
-const status = [
-    <Alert message="已完成" type="success" showIcon />,
-    <Alert message="待办" type="info" showIcon />,
-    <Alert message="已取消" type="error" showIcon />,
-]
-
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-  },
-  {
-    title: '状态',
-    dataIndex: 'status',
-    render:(_,record) => {return status[record.status]}
-  },
-  {
-    title: '修改状态',
-    render:() => [
-      <a>待办 </a>,
-      <a>已完成 </a>,
-      <a>已取消 </a>,
-    ]
-  },
-]
-
-
-
-const Todo = () => {
-  let [data, setData] = useState([])
-  useEffect(async () => {
-    const resData = await getTodoLists()
-    setData(resData)
-  }, [])
-
+import { LineLayer, AMapScene } from '@antv/l7-react';
+import React from "react";
+export default React.memo(function Map() {
+  const [data, setData] = React.useState();
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://gw.alipayobjects.com/os/basement_prod/32e1f3ab-8588-46cb-8a47-75afb692117d.json',
+      );
+      const raw = await response.json();
+      setData(raw);
+    };
+    fetchData();
+  }, []);
   return (
-    <ProTable
-      columns={columns}
-      rowKey="key"
-      pagination={{
-        showQuickJumper: true,
-      }}
-      search={false}
-      dateFormatter="string"
-      headerTitle="待办事项列表"
-      //dataSource={data}
-      request={async () => ({data: await getTodoLists()})}
-      toolBarRender={() => [
-        <Button type="primary" key="primary">
-          <PlusOutlined />新建
-        </Button>,
-      ]}
-    />
+    <>
+      <AMapScene
+        map={{
+          center: [110.19382669582967, 50.258134],
+          pitch: 0,
+          style: 'dark',
+          zoom: 1,
+        }}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        {data && (
+          <LineLayer
+            key={'2'}
+            source={{
+              data,
+            }}
+            size={{
+              values: 1,
+            }}
+            color={{
+              values: '#fff',
+            }}
+            shape={{
+              values: 'line',
+            }}
+            style={{
+              opacity: 1,
+            }}
+          />
+        )}
+      </AMapScene>
+    </>
   );
-};
-
-export default Todo;
+});
