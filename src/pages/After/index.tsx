677373c669e4c5b9d66cac80cea6036b720bb075
oneluxyou <77523920+oneluxyou@users.noreply.github.com>
2021-11-20 12:33:36 +0800

    Update index.tsx

--- src/pages/After/index.tsx
+++ src/pages/After/index.tsx
@@ -234,27 +234,27 @@ const TableList: React.FC = () => {
             name="店铺"
             label="店铺"
             placeholder="请输入店铺"
-            valueEnum={{
-              赫曼:'赫曼',
-              信盒:'信盒',
-              宫本:'宫本',
-              森月:'森月',
-              维禄:'维禄',
-              玲琅:'玲琅',
-              信盒_法国:'信盒-法国',
-              信盒_意大利:'信盒-意大利',
-              信盒_西班牙:'信盒-西班牙',
-              Wayfair_信盒:'Wayfair-信盒',
-              Wayfair_维禄:'Wayfair-维禄',
-              Walmart_优瑞斯特:'Walmart-优瑞斯特',
-              Walmart_赫曼:'Walmart-赫曼',
-              Walmart_信盒:'Walmart-信盒',
-              Walmart_宫本:'Walmart-宫本',
-              eBay_玲琅:'eBay-玲琅',
-              eBay_治润:'eBay-治润',
-              eBay_雅秦:'eBay-雅秦',
-              Nextfur_Shopify:'Nextfur-Shopify',
-            }}
+                            valueEnum={{
+                  '赫曼':'赫曼',
+                  '信盒':'信盒',
+                  '宫本':'宫本',
+                  '森月':'森月',
+                  '维禄':'维禄',
+                  '玲琅':'玲琅',
+                  '信盒-法国':'信盒-法国',
+                  '信盒-意大利':'信盒-意大利',
+                  '信盒-西班牙':'信盒-西班牙',
+                  'Wayfair-信盒':'Wayfair-信盒',
+                  'Wayfair-维禄':'Wayfair-维禄',
+                  'Walmart-优瑞斯特':'Walmart-优瑞斯特',
+                  'Walmart-赫曼':'Walmart-赫曼',
+                  'Walmart-信盒':'Walmart-信盒',
+                  'Walmart-宫本':'Walmart-宫本',
+                  'eBay-玲琅':'eBay-玲琅',
+                  'eBay-治润':'eBay-治润',
+                  'eBay-雅秦':'eBay-雅秦',
+                  'Nextfur-Shopify':'Nextfur-Shopify',
+                }}
             rules={[{ required: true, message: '请输入店铺!' }]}
           />
           <ProFormText width="md" name="订单号" label="订单号" rules={[{ required: true, message: '请输入订单号!' }]}/>
@@ -265,15 +265,15 @@ const TableList: React.FC = () => {
             label="处理方式"
             placeholder="请输入处理方式"
             valueEnum={{
-              Wait_Reply:'Wait Reply',
-              Cancel_Order:'Cancel Order',
-              Closed:'Closed',
-              Replacement:'Replacement',
-              Refund:'Refund',
-              Used:'Used',
-              Refund_and_Replacement:'Refund and Replacement',
-              Delivery_Consultation:'Delivery Consultation',
-            }}
+                  'Wait Reply':'Wait Reply',
+                  'Cancel Order':'Cancel Order',
+                  'Closed':'Closed',
+                  'Replacement':'Replacement',
+                  'Refund':'Refund',
+                  'Used':'Used',
+                  'Refund and Replacement':'Refund and Replacement',
+                  'Delivery Consultation':'Delivery Consultation',
+                }}
             rules={[{ required: true, message: '请输入处理方式!' }]}
           />
           <ProFormDigit
@@ -291,41 +291,41 @@ const TableList: React.FC = () => {
             label="原因"
             placeholder="请输入原因"
             valueEnum={{
-              少配件:'少配件',
-              外观问题:'外观问题（安装前）',
-              组装问题:'组装问题（安装中）',
-              结构问题:'结构问题（安装后）',
-              其它质量问题:'其它质量问题',
-              质量少配件:'质量&少配件',
-              运输破损:'运输破损',
-              仓库:'仓库',
-              快递:'快递',
-              买家:'买家',
-              销售:'销售',
-            }}
+                  '少配件':'少配件',
+                  '外观问题（安装前）':'外观问题（安装前）',
+                  '组装问题（安装中）':'组装问题（安装中）',
+                  '结构问题（安装后）':'结构问题（安装后）',
+                  '其它质量问题':'其它质量问题',
+                  '质量&少配件':'质量&少配件',
+                  '运输破损':'运输破损',
+                  '仓库':'仓库',
+                  '快递':'快递',
+                  '买家':'买家',
+                  '销售':'销售',
+                }}
             rules={[{ required: true, message: '请输入原因!' }]}
           />
           <ProFormSelect
             width="md"
             name="Replacement"
             label="Replacement"
-            valueEnum={{
-              RP_国外新件:'RP-国外新件',
-              RP_国外配件:'RP-国外配件',
-              RP_国外退件:'RP-国外退件',
-              RP_国内补寄配件:'RP-国内补寄配件',
-            }}
+                valueEnum={{
+                  'RP-国外新件':'RP-国外新件',
+                  'RP-国外配件':'RP-国外配件',
+                  'RP-国外退件':'RP-国外退件',
+                  'RP-国内补寄配件':'RP-国内补寄配件',
+                }}
           />
           <ProFormSelect
             width="md"
             name="Used"
             label="退件"
-            valueEnum={{
-              买家承担:'买家承担',
-              卖家承担_上门取件:'卖家承担-上门取件',
-              卖家承担_退货标签:'卖家承担-退货标签',
-              拦截:'拦截',
-            }}
+valueEnum={{
+                  '买家承担':'买家承担',
+                  '卖家承担-上门取件':'卖家承担-上门取件',
+                  '卖家承担-退货标签':'卖家承担-退货标签',
+                  '拦截':'拦截',
+                }}
           />
           <ProFormText width="md" name="售后反馈" label="售后反馈" />
           <ProFormText width="xl" name="备注" label="备注" />
