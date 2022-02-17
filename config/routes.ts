e3f01f0fbffa33ec85b79836448db8a9677eb3b2
ClydeZwei <1257031700@qq.com>
2022-02-17 14:20:47 +0800

    只售后

--- config/routes.ts
+++ config/routes.ts
@@ -31,63 +31,63 @@
       },
     ],
   },
-  {
-    path: '/match',
-    name: '匹配表',
-    icon: 'crown',
-    routes: [
-      {
-        path: '/match/index',
-        name: '对应信息更新',
-        icon: 'smile',
-        component: './match/index',
-      },
-      {
-        component: './404',
-      },
-    ],
-  },
-  {
-    path: '/dashboard',
-    name: '销售预报',
-    icon: 'crown',
-    routes: [
-      // {
-      //   path: '/dashboard/saleTotal',
-      //   name: '销售汇总',
-      //   icon: 'smile',
-      //   component: './dashboard/saleTotal/index',
-      //   access: 'Skusaler',
-      // },
-      {
-        path: '/dashboard/skuTotal',
-        name: '各sku序号销售情况',
-        icon: 'smile',
-        component: './dashboard/skuTotal/index',
-      },
-      {
-        path: '/dashboard/saleRank',
-        name: '销售报表',
-        icon: 'smile',
-        component: './dashboard/saleRank/index',
-      },
-      {
-        component: './404',
-      },
-    ],
-  },
-  {
-    path: '/welcome',
-    name: '周转表',
-    icon: 'TableOutlined',
-    component: './Table',
-  },
-  {
-    path: '/inventory',
-    name: '库存分布表',
-    icon: 'smile',
-    component: './inventory',
-  },
+  // {
+  //   path: '/match',
+  //   name: '匹配表',
+  //   icon: 'crown',
+  //   routes: [
+  //     {
+  //       path: '/match/index',
+  //       name: '对应信息更新',
+  //       icon: 'smile',
+  //       component: './match/index',
+  //     },
+  //     {
+  //       component: './404',
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/dashboard',
+  //   name: '销售预报',
+  //   icon: 'crown',
+  //   routes: [
+  //     // {
+  //     //   path: '/dashboard/saleTotal',
+  //     //   name: '销售汇总',
+  //     //   icon: 'smile',
+  //     //   component: './dashboard/saleTotal/index',
+  //     //   access: 'Skusaler',
+  //     // },
+  //     {
+  //       path: '/dashboard/skuTotal',
+  //       name: '各sku序号销售情况',
+  //       icon: 'smile',
+  //       component: './dashboard/skuTotal/index',
+  //     },
+  //     {
+  //       path: '/dashboard/saleRank',
+  //       name: '销售报表',
+  //       icon: 'smile',
+  //       component: './dashboard/saleRank/index',
+  //     },
+  //     {
+  //       component: './404',
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/welcome',
+  //   name: '周转表',
+  //   icon: 'TableOutlined',
+  //   component: './Table',
+  // },
+  // {
+  //   path: '/inventory',
+  //   name: '库存分布表',
+  //   icon: 'smile',
+  //   component: './inventory',
+  // },
   {
     path: '/todo',
     icon: 'HeatMapOutlined',
